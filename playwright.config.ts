94eb5362e8b052697d2d434d0d5a5a8be0b101d7
Slacknwork <tphcmtrung@gmail.com>
2026-03-02 23:32:01 +0700

    disable auto start report

--- playwright.config.ts
+++ playwright.config.ts
@@ -43,7 +43,7 @@ export default defineConfig({
       embedAttachments: true,
       outputFolder: 'reports',
       minifyAssets: true,
-      startServer: true,
+      startServer: false,
     }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
